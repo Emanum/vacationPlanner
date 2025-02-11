85e09fdf0b712a9c7b275bb32794a88c3ffbea21
Manuel Eiweck <manuel.eiweck@emanum.dev>
2025-02-11 19:14:31 +0100

    base tutorial

--- app.vue
+++ app.vue
@@ -1,6 +1,10 @@
+<script setup>
+const user = useSupabaseUser()
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="container" style="padding: 50px 0 100px 0">
+    <Account v-if="user" />
+    <Auth v-else />
   </div>
 </template>
